6641b8d16835e480267cc6b34ffdac4ec422851d
Chris Gilbert <chrisgilbert4@icloud.com>
2018-11-05 18:52:47 +0000

    tests cleaned up

--- spec/oystercard_status_spec.rb
+++ spec/oystercard_status_spec.rb
@@ -2,35 +2,36 @@ require 'oystercard_status'
 
 describe OysterCardStatus do
 
+  let(:oystercard_status) { described_class.new }
+
   let(:oystercard) { double :oystercard, :in_journey => false }
 
   describe '#initialize' do
-    it 'sets in journey to false by default' do
-      expect(subject.in_journey?).to eq false
+    it 'sets in journey status to false by default' do
+      expect(oystercard_status.in_journey).to eq false
     end
 
     it 'sets the minimum fare to touch in with at a default value of 1' do
-      expect(subject.min_fare).to eq OysterCardStatus::MIN_FARE
+      expect(oystercard_status.min_fare).to eq OysterCardStatus::MIN_FARE
     end
   end
 
   describe '#touch_in' do
     it 'changes oystercard status to in journey' do
-      oystercard = OysterCard.new
-      expect(oystercard.card_status.touch_in(1)).to eq oystercard.card_status
+      oystercard_status.touch_in(1)
+      expect(oystercard_status.in_journey).to eq true
     end
 
     it 'user is not allowed to touch if there is less than £1 in the balance' do
-      oystercard = OysterCard.new
       message = 'You dont have suffient balance to touch in'
-      expect { oystercard.card_status.touch_in(oystercard.balance) }.to raise_error message
+      expect { oystercard_status.touch_in(0) }.to raise_error message
     end
   end
 
   describe '#touch_out' do
     it 'changes oystercard status to not in journey' do
-      oystercard = OysterCard.new
-      expect(oystercard.card_status.touch_out).to eq oystercard.card_status
+      oystercard_status.touch_out
+      expect(oystercard_status.in_journey).to eq false
     end
   end
 
@@ -41,7 +42,5 @@ describe OysterCardStatus do
       expect(subject.in_journey?).to eq false
     end
   end
-
-
 
 end
