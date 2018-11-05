70bf8f00384662f6e61085dee8d2dca8de84d3b4
Chris Gilbert <chrisgilbert4@icloud.com>
2018-11-05 19:43:20 +0000

    tests now using change by

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -7,16 +7,14 @@ class OysterCard
   DEFAULT_BALANCE = 0
   MAX_LIMIT = 90
 
-  def initialize(default_balance = DEFAULT_BALANCE, max_limit = MAX_LIMIT, card_status = OysterCardStatus.new)
+  def initialize(default_balance = DEFAULT_BALANCE, max_limit = MAX_LIMIT)
     @balance = default_balance
     @max_limit = max_limit
-    @card_status = card_status
   end
 
   def top_up(value)
     fail "Your balance is currently #{@balance} and your limit is #{OysterCard::MAX_LIMIT}" if exceed_max_limit?(value)
     @balance += value
-    value
   end
 
   def deduct(value)
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,12 +4,6 @@ describe OysterCard do
 
   let(:oystercard) { described_class.new }
 
-  describe '#initialize' do
-    it 'has a card status by default' do
-      expect(oystercard.card_status).to be_a_kind_of OysterCardStatus
-    end
-  end
-
   describe '#balance' do
     it 'returns the user balance' do
       expect(oystercard.balance).to eq 0
@@ -20,31 +14,25 @@ describe OysterCard do
     end
 
     it 'Adds the top up value to the balance' do
-      oystercard.top_up(10)
-      expect(oystercard.balance).to eq 10
+      expect { oystercard.top_up(3) }.to change{ oystercard.balance}.by 3
     end
 
     it 'doesn"t allow the user to top up above card limit (£90)' do
-      expect { oystercard.top_up(95) }.to raise_error "Your balance is currently #{oystercard.balance} and your limit is #{OysterCard::MAX_LIMIT}"
+      message = "Your balance is currently #{oystercard.balance} and your limit is #{OysterCard::MAX_LIMIT}"
+      expect { oystercard.top_up(95) }.to raise_error message
     end
 
-    it 'doesnt" allow the user to top up the balance above the max limit' do
+    it 'doesn"t allow the user to top up the balance above the max limit' do
       oystercard.top_up(90)
-      expect { oystercard.top_up(5) }.to raise_error "Your balance is currently #{oystercard.balance} and your limit is #{OysterCard::MAX_LIMIT}"
+      message = "Your balance is currently #{oystercard.balance} and your limit is #{OysterCard::MAX_LIMIT}"
+      expect { oystercard.top_up(5) }.to raise_error message
     end
   end
 
     describe '#deduct' do
-      it ' Allows a fair to be deducted from the card' do
+      it 'allows a fare to be deducted from the balance' do
         oystercard.top_up(10)
-        expect(oystercard.deduct(7)).to eq 7
+        expect { oystercard.deduct(3) }.to change{ oystercard.balance }.by -3
       end
-
-      it 'Deducts the fair from the balance' do
-        oystercard.top_up(10)
-        oystercard.deduct(7)
-        expect(oystercard.balance).to eq 3
-      end
-
     end
 end
