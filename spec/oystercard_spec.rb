009b56b5032509c5ae1b9b835e6d60e25fb102e9
Chris Gilbert <chrisgilbert4@icloud.com>
2018-11-05 18:53:56 +0000

    stubs implemented

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,46 +2,48 @@ require 'oystercard'
 
 describe OysterCard do
 
+  let(:oystercard) { described_class.new }
+
   describe '#initialize' do
     it 'has a card status by default' do
-      expect(subject.card_status).to be_a_kind_of OysterCardStatus
+      expect(oystercard.card_status).to be_a_kind_of OysterCardStatus
     end
   end
 
   describe '#balance' do
     it 'returns the user balance' do
-      expect(subject.balance).to eq 0
+      expect(oystercard.balance).to eq 0
     end
 
     it 'allows the user to top up their balance' do
-      expect(subject.top_up(5)).to eq 5
+      expect(oystercard.top_up(5)).to eq 5
     end
 
     it 'Adds the top up value to the balance' do
-      subject.top_up(10)
-      expect(subject.balance).to eq 10
+      oystercard.top_up(10)
+      expect(oystercard.balance).to eq 10
     end
 
     it 'doesn"t allow the user to top up above card limit (£90)' do
-      expect { subject.top_up(95) }.to raise_error "Your balance is currently #{subject.balance} and your limit is #{OysterCard::MAX_LIMIT}"
+      expect { oystercard.top_up(95) }.to raise_error "Your balance is currently #{oystercard.balance} and your limit is #{OysterCard::MAX_LIMIT}"
     end
 
     it 'doesnt" allow the user to top up the balance above the max limit' do
-      subject.top_up(90)
-      expect { subject.top_up(5) }.to raise_error "Your balance is currently #{subject.balance} and your limit is #{OysterCard::MAX_LIMIT}"
+      oystercard.top_up(90)
+      expect { oystercard.top_up(5) }.to raise_error "Your balance is currently #{oystercard.balance} and your limit is #{OysterCard::MAX_LIMIT}"
     end
   end
 
     describe '#deduct' do
       it ' Allows a fair to be deducted from the card' do
-        subject.top_up(10)
-        expect(subject.deduct(7)).to eq 7
+        oystercard.top_up(10)
+        expect(oystercard.deduct(7)).to eq 7
       end
 
       it 'Deducts the fair from the balance' do
-        subject.top_up(10)
-        subject.deduct(7)
-        expect(subject.balance).to eq 3
+        oystercard.top_up(10)
+        oystercard.deduct(7)
+        expect(oystercard.balance).to eq 3
       end
 
     end
